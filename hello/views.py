8fd86754468986835ced83de800380f2faea305d
Ramadhar Mishra <36169952+ramadharmishra@users.noreply.github.com>
2018-12-16 13:18:40 -0800

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from simple_salesforce import Salesforce
 
 from .models import Greeting
+
+sf = Salesforce(username='ramadhar.mishra@gmail.com', password='Ram@123455', security_token='zYrbF3Xi0VdlptK01MrZKaDX')
 
 # Create your views here.
 def index(request):
